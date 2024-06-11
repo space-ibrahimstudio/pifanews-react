96129e6ecf019bc39292c6185fb9e636195f2f55
Maulana Malik Ibrahim <ibrahimcpersonal@gmail.com>
2024-06-11 18:48:54 +0700

    feat: add reset scrolling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React, { Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { Suspense, useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import HomePage from "./pages/home";
 import CategoryPage from "./pages/category";
 
 function App() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <Suspense>
       <Routes>
