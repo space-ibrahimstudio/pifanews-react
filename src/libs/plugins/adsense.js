0962b1029a29dc5779871c2a514954b3adcefe73
100percentibrahim <ibrahimcpersonal@gmail.com>
2024-10-02 22:05:09 +0700

    test: test fix CI detection

--- src/libs/plugins/adsense.js
+++ src/libs/plugins/adsense.js
@@ -3,22 +3,30 @@ import React, { useEffect } from "react";
 const capub = process.env.REACT_APP_CA_PUB_MAIN;
 
 const AdSense = () => {
+  const cistatus = process.env.REACT_APP_CI;
+
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${capub}`;
-    script.async = true;
-    script.crossOrigin = "anonymous";
-    document.body.appendChild(script);
+    if (cistatus !== "true") {
+      const script = document.createElement("script");
+      script.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${capub}`;
+      script.async = true;
+      script.crossOrigin = "anonymous";
+      document.body.appendChild(script);
 
-    const adsInitialized = document.querySelectorAll(".adsbygoogle[data-ad-status='done']").length === 0;
-    if (adsInitialized) {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
+      const adsInitialized = document.querySelectorAll(".adsbygoogle[data-ad-status='done']").length === 0;
+      if (adsInitialized) {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+      }
+
+      return () => {
+        document.body.removeChild(script);
+      };
     }
+  }, [cistatus, capub]);
 
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
+  if (cistatus === "true") {
+    return null;
+  }
 
   return <ins className="adsbygoogle" style={{ display: "block", width: "100%", height: "auto" }} data-ad-client={capub} data-ad-slot="1436038114" data-ad-format="auto" data-full-width-responsive="true"></ins>;
 };
