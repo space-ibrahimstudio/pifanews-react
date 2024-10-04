f51b3e062b2ff544d0af4350b6bab631d17dae64
100percentibrahim <ibrahimcpersonal@gmail.com>
2024-10-04 22:17:38 +0700

    test: test adsense with SPA

--- src/libs/plugins/adsense.js
+++ src/libs/plugins/adsense.js
@@ -1,10 +1,13 @@
-import React from "react";
-import { Adsense } from "@ctrl/react-adsense";
+import React, { useEffect } from "react";
 
 const capub = process.env.REACT_APP_CA_PUB_MAIN;
 
 const AdSense = () => {
-  return <Adsense className="pifaNetAd" client={capub} slot="1436038114" adTest="on" />;
+  useEffect(() => {
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  }, []);
+
+  return <ins className="adsbygoogle" style={{ display: "block", width: "100%", height: "auto" }} data-ad-client={capub} data-ad-slot="1436038114" data-ad-format="auto" data-full-width-responsive="true"></ins>;
 };
 
 export default AdSense;
