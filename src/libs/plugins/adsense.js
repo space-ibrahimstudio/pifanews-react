d206ef78f85e375732f7cbf754c5a42eeb4bbb49
100percentibrahim <ibrahimcpersonal@gmail.com>
2024-10-05 23:05:26 +0700

    fix: fix CSP & ad width

--- src/libs/plugins/adsense.js
+++ src/libs/plugins/adsense.js
@@ -4,26 +4,28 @@ const capub = process.env.REACT_APP_CA_PUB_MAIN;
 
 const AdSense = () => {
   const isCrawl = typeof window !== "undefined" && window.navigator.userAgent === "IbrahimStudio";
+
   useEffect(() => {
     if (!isCrawl) {
       const existingScript = document.querySelector('script[src*="adsbygoogle.js"]');
       if (!existingScript) {
         const script = document.createElement("script");
-        script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9744736888740959";
+        script.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${capub}`;
         script.async = true;
         script.crossOrigin = "anonymous";
         document.body.appendChild(script);
-      }
-      try {
-        window.adsbygoogle = window.adsbygoogle || [];
-        window.adsbygoogle.push({});
-      } catch (e) {
-        console.error("Adsbygoogle error:", e);
+        script.onload = () => {
+          const adsbygoogle = window.adsbygoogle || [];
+          adsbygoogle.push({});
+        };
+      } else {
+        const adsbygoogle = window.adsbygoogle || [];
+        adsbygoogle.push({});
       }
     }
   }, [isCrawl]);
 
-  return <ins className="adsbygoogle" style={{ display: "block" }} data-ad-client="ca-pub-9744736888740959" data-ad-slot="5581971140" data-ad-format="auto" data-full-width-responsive="true"></ins>;
+  return <ins className="adsbygoogle" style={{ display: "block" }} data-ad-client={capub} data-ad-slot="5581971140" data-ad-format="auto" data-full-width-responsive="true"></ins>;
 };
 
 export default AdSense;
