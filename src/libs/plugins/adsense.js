d5a933928296365c20159f4688714db2167c7213
100percentibrahim <ibrahimcpersonal@gmail.com>
2024-10-05 18:39:37 +0700

    fix: fix adsense

--- src/libs/plugins/adsense.js
+++ src/libs/plugins/adsense.js
@@ -7,7 +7,7 @@ const AdSense = () => {
     (window.adsbygoogle = window.adsbygoogle || []).push({});
   }, []);
 
-  return <ins className="adsbygoogle" style={{ display: "block", width: "100%", height: "auto" }} data-ad-client={capub} data-ad-slot="1436038114" data-ad-format="auto" data-full-width-responsive="true"></ins>;
+  return <ins className="adsbygoogle" style={{ display: "block", width: "100%", height: "auto" }} data-ad-client="ca-pub-9744736888740959" data-ad-slot="5581971140" data-ad-format="auto" data-full-width-responsive="true"></ins>;
 };
 
 export default AdSense;
