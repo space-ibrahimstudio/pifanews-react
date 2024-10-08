1c38cbed655c71b3396cd1ba088e339c9bb522ca
100percentibrahim <ibrahimcpersonal@gmail.com>
2024-10-08 21:18:50 +0700

    fix: try fix modal-root

--- src/components/layout/popover.js
+++ src/components/layout/popover.js
@@ -1,12 +1,13 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { Fragment, useState, useRef, useEffect } from "react";
 import { createPortal } from "react-dom";
 import styles from "./styles/popover.module.css";
 
 const modalRoot = document.getElementById("modal-root") || document.body;
 
 const PopOver = ({ onSubmit, onClose, children }) => {
-  const [isClosing, setIsClosing] = useState(false);
+  const compid = `popover-form`;
   const ref = useRef(null);
+  const [isClosing, setIsClosing] = useState(false);
 
   const handleClickOutside = (e) => {
     if (ref.current && !ref.current.contains(e.target)) {
@@ -45,10 +46,29 @@ const PopOver = ({ onSubmit, onClose, children }) => {
   }, [isClosing]);
 
   const modalElement = (
-    <main className={styles.formScroll}>
-      <section className={`${styles.formScreen} ${isClosing ? styles.close : ""}`}>
-        <form ref={ref} className={`${styles.form} ${isClosing ? styles.close : ""}`} style={{ maxWidth: "var(--pixel-550)" }} onSubmit={onSubmit}>
-          {children}
+    <main id={compid} className={styles.formScroll}>
+      <section id={`${compid}-wrap`} className={`${styles.formScreen} ${isClosing ? styles.close : ""}`}>
+        <form id={`${compid}-wrap-form`} ref={ref} className={`${styles.form} ${isClosing ? styles.close : ""}`} style={{ maxWidth: "var(--pixel-550)" }} onSubmit={onSubmit}>
+          {React.Children.map(children, (child) => {
+            if (React.isValidElement(child)) {
+              if (child.type === Fragment) {
+                return (
+                  <Fragment>
+                    {React.Children.map(child.props.children, (fragmentChild) => {
+                      if (React.isValidElement(fragmentChild)) {
+                        const combinedId = fragmentChild.props.id ? `${compid}-wrap-form-${fragmentChild.props.id}` : `${compid}-wrap-form`;
+                        return React.cloneElement(fragmentChild, { id: combinedId });
+                      }
+                      return fragmentChild;
+                    })}
+                  </Fragment>
+                );
+              }
+              const combinedId = child.props.id ? `${compid}-wrap-form-${child.props.id}` : `${compid}-wrap-form`;
+              return React.cloneElement(child, { id: combinedId });
+            }
+            return child;
+          })}
         </form>
       </section>
     </main>
