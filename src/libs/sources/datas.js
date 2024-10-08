6a306cd6061752313859e35f9864b22f7f2fabb8
100percentibrahim <ibrahimcpersonal@gmail.com>
2024-10-08 23:52:25 +0700

    fix: fix undefined id

--- src/libs/sources/datas.js
+++ src/libs/sources/datas.js
@@ -109,37 +109,43 @@ export const getStaticPosts = () => {
 export const getInfographicPosts = () => {
   const posts = [
     {
+      id: "1",
       title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut lectus dui. Nullam vulputate commodo euismod.",
       image: "/img/graphic/art-1.jpg",
       count: "4",
       status: "BARU",
     },
     {
+      id: "2",
       title: "In sodales imperdiet nisl vel scelerisque. Duis venenatis fermentum lacinia.",
       image: "/img/graphic/art-2.jpg",
       count: "7",
       status: "UPDATE",
     },
     {
+      id: "3",
       title: "Quisque in porta dui, vel dictum odio. Phasellus ac tellus non neque pellentesque laoreet.",
       image: "/img/graphic/art-3.jpg",
       count: "1",
       status: "BARU",
     },
-    { title: "Aenean sit amet elit sit amet sem ornare blandit.", image: "/img/graphic/art-4.jpg", count: "12", status: "" },
+    { id: "", title: "Aenean sit amet elit sit amet sem ornare blandit.", image: "/img/graphic/art-4.jpg", count: "12", status: "" },
     {
+      id: "4",
       title: "Quisque in augue id ligula auctor mattis at at diam. Sed at rutrum nisl.",
       image: "/img/graphic/art-5.jpg",
       count: "2",
       status: "UPDATE",
     },
     {
+      id: "5",
       title: "Quisque tempus, arcu vel molestie aliquam, augue enim tempor odio, eu malesuada tortor lacus quis nisi.",
       image: "/img/graphic/art-6.jpg",
       count: "8",
       status: "",
     },
     {
+      id: "6",
       title: "Integer ut mollis sapien. Duis pellentesque leo pretium pretium egestas.",
       image: "/img/graphic/art-7.jpg",
       count: "23",
