1244e79216ebd46272181cafe426f2530f53aab1
100percentibrahim <ibrahimcpersonal@gmail.com>
2024-10-12 05:53:28 +0700

    fix: fix incorrect import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { createRoot, hydrateRoot } from "react-dom/client";
+import ReactDOM, { createRoot, hydrateRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { createGenerateId, JssProvider, SheetsRegistry } from "react-jss";
