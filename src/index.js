f573242a66133569fa218e764657628f2eea5cf8
100percentibrahim <ibrahimcpersonal@gmail.com>
2024-10-12 05:49:14 +0700

    chore: disable static gen

--- src/index.js
+++ src/index.js
@@ -28,82 +28,82 @@ const sendtoLog = ({ name, delta, id }) => {
   console.log(`[DEV] ${name}:`, { id, delta });
 };
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <HelmetProvider>
-//       <IbrahimStudioProvider>
-//         <BrowserRouter>
-//           <LoadingProvider>
-//             <ApiProvider>
-//               <AuthProvider>
-//                 <App />
-//               </AuthProvider>
-//             </ApiProvider>
-//           </LoadingProvider>
-//         </BrowserRouter>
-//       </IbrahimStudioProvider>
-//     </HelmetProvider>
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <IbrahimStudioProvider>
+        <BrowserRouter>
+          <LoadingProvider>
+            <ApiProvider>
+              <AuthProvider>
+                <App />
+              </AuthProvider>
+            </ApiProvider>
+          </LoadingProvider>
+        </BrowserRouter>
+      </IbrahimStudioProvider>
+    </HelmetProvider>
+  </React.StrictMode>
+);
 
-const root = document.getElementById("root");
-if (root && root.hasChildNodes()) {
-  hydrateRoot(
-    root,
-    <React.StrictMode>
-      <HelmetProvider>
-        <IbrahimStudioProvider>
-          <BrowserRouter>
-            <LoadingProvider>
-              <ApiProvider>
-                <AuthProvider>
-                  <App />
-                </AuthProvider>
-              </ApiProvider>
-            </LoadingProvider>
-          </BrowserRouter>
-        </IbrahimStudioProvider>
-      </HelmetProvider>
-    </React.StrictMode>,
-    () => {
-      const reactSnapStyles = document.getElementById("react-snap-styles");
-      reactSnapStyles?.parentNode?.removeChild(reactSnapStyles);
-    }
-  );
-} else {
-  const registry = new SheetsRegistry();
-  const generateId = createGenerateId();
-  createRoot(root, () => {
-    if (navigator.userAgent === "IbrahimStudio") {
-      const badStyles = document.querySelectorAll("[data-jss]");
-      badStyles.forEach((cssStyle) => cssStyle.parentNode?.removeChild(cssStyle));
-      const style = document.createElement("style");
-      style.innerHTML = registry.toString();
-      style.setAttribute("id", "react-snap-styles");
-      const head = document.querySelector("head");
-      head.appendChild(style);
-    }
-  }).render(
-    <JssProvider registry={registry} generateId={generateId}>
-      <React.StrictMode>
-        <HelmetProvider>
-          <IbrahimStudioProvider>
-            <BrowserRouter>
-              <LoadingProvider>
-                <ApiProvider>
-                  <AuthProvider>
-                    <App />
-                  </AuthProvider>
-                </ApiProvider>
-              </LoadingProvider>
-            </BrowserRouter>
-          </IbrahimStudioProvider>
-        </HelmetProvider>
-      </React.StrictMode>
-    </JssProvider>
-  );
-}
+// const root = document.getElementById("root");
+// if (root && root.hasChildNodes()) {
+//   hydrateRoot(
+//     root,
+//     <React.StrictMode>
+//       <HelmetProvider>
+//         <IbrahimStudioProvider>
+//           <BrowserRouter>
+//             <LoadingProvider>
+//               <ApiProvider>
+//                 <AuthProvider>
+//                   <App />
+//                 </AuthProvider>
+//               </ApiProvider>
+//             </LoadingProvider>
+//           </BrowserRouter>
+//         </IbrahimStudioProvider>
+//       </HelmetProvider>
+//     </React.StrictMode>,
+//     () => {
+//       const reactSnapStyles = document.getElementById("react-snap-styles");
+//       reactSnapStyles?.parentNode?.removeChild(reactSnapStyles);
+//     }
+//   );
+// } else {
+//   const registry = new SheetsRegistry();
+//   const generateId = createGenerateId();
+//   createRoot(root, () => {
+//     if (navigator.userAgent === "IbrahimStudio") {
+//       const badStyles = document.querySelectorAll("[data-jss]");
+//       badStyles.forEach((cssStyle) => cssStyle.parentNode?.removeChild(cssStyle));
+//       const style = document.createElement("style");
+//       style.innerHTML = registry.toString();
+//       style.setAttribute("id", "react-snap-styles");
+//       const head = document.querySelector("head");
+//       head.appendChild(style);
+//     }
+//   }).render(
+//     <JssProvider registry={registry} generateId={generateId}>
+//       <React.StrictMode>
+//         <HelmetProvider>
+//           <IbrahimStudioProvider>
+//             <BrowserRouter>
+//               <LoadingProvider>
+//                 <ApiProvider>
+//                   <AuthProvider>
+//                     <App />
+//                   </AuthProvider>
+//                 </ApiProvider>
+//               </LoadingProvider>
+//             </BrowserRouter>
+//           </IbrahimStudioProvider>
+//         </HelmetProvider>
+//       </React.StrictMode>
+//     </JssProvider>
+//   );
+// }
 
 if (process.env.NODE_ENV === "production") {
   reportWebVitals(sendtoGA);
